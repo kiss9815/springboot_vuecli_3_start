d72232d29e9c265a03f39d112e5d88c5afd3e664
juntae <kiss9815@gmail.com>
2019-05-08 14:23:11 +0900

    시작

---
+++ vue_3_start/src/main/java/com/example/demo/Vue3StartApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vue3StartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vue3StartApplication.class, args);
+	}
+
+}
